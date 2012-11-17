11649addcdde59192ef093c56b65b7b034f89a19
nether <jorge.fd@gmail.com>
2012-11-18 00:29:51 +0100

    Create rails app

---
+++ torneo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torneo::Application.config.secret_token = '9800bedaa56a9d37808d7c7e77cf89e3903e5dc651b7cf671cc71a74b2deb0a2dc095337ccc8bb988bfecee1a55dc40cd1f1dc8ae25eb44fd795197159fc99fe'
